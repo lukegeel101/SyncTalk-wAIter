53c1b2fefd0653c5e1011f27a55d1712ea5c4004
Luke Geel <geelluke@gmail.com>
2025-10-20 11:04:41 -0400

    Create fix_cpp_standard.py

---
+++ fix_cpp_standard.py
@@ -0,0 +1,7 @@
+import glob 
+	files = glob.glob('*/*.py') for f in files: 
+		with open(f) as file: 
+			lines = file.readlines() 
+		lines = [l.replace('-std=c++14', '-std=c++17') for l in lines] 
+		with open(f, 'w') as file: file.writelines(lines) 
+print(f"Fixed C++ standard in {len(files)} files")
